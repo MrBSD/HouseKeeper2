15346b577301586a2f8b080f8dcf8d4dba277870
MrBSD <MrBSD82@gmail.com>
2017-11-01 12:05:09 +0200

    Add project files.

---
+++ HouseKeeper2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HouseKeeper2.Startup))]
+namespace HouseKeeper2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
