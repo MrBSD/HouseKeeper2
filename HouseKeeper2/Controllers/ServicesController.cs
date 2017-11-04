7bf1990a74e9baaa263cfd18221d773682309627
MrBSD <MrBSD82@gmail.com>
2017-11-04 17:24:29 +0200

    Implemented adding of new service

--- HouseKeeper2/Controllers/ServicesController.cs
+++ HouseKeeper2/Controllers/ServicesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HouseKeeper2.Core.Models;
 using HouseKeeper2.Core.Repositories;
 using HouseKeeper2.Persistence;
 using HouseKeeper2.Persistence.Repositories;
@@ -24,5 +25,21 @@ namespace HouseKeeper2.Controllers
             var services = _context.Services.ToList();
             return View(services);
         }
+
+        // GET: Services/create
+        public ActionResult Create()
+        {
+            var service = new Service();
+            return View("ServiceForm", service);
+        }
+
+        //POST: Services/create
+        public ActionResult Save(Service service)
+        {
+            _context.Services.Add(service);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Services");
+        }
+
     }
 }
