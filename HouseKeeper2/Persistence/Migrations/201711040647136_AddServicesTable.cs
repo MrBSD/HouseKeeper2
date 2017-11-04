a0d6a49e6074e3d880b7b4f010efcff6cff58238
MrBSD <MrBSD82@gmail.com>
2017-11-04 08:52:27 +0200

    Added Services Table

---
+++ HouseKeeper2/Persistence/Migrations/201711040647136_AddServicesTable.cs
@@ -0,0 +1,26 @@
+namespace HouseKeeper2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddServicesTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Services",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Services");
+        }
+    }
+}
