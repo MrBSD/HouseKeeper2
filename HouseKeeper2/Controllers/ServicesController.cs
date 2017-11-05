d62cb9bd623ae7613870b637a384128bcd9a0e1d
MrBSD <MrBSD82@gmail.com>
2017-11-05 14:24:25 +0200

    Implemented editing, deleting of Services. Made it async

--- HouseKeeper2/Controllers/ServicesController.cs
+++ HouseKeeper2/Controllers/ServicesController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using HouseKeeper2.Core.Models;
@@ -18,28 +20,61 @@ namespace HouseKeeper2.Controllers
         {
             _context = new ApplicationDbContext();
         }
-        
-        // GET: Services
-        public ActionResult Index()
-        {
-            var services = _context.Services.ToList();
-            return View(services);
-        }
-
+         
         // GET: Services/create
         public ActionResult Create()
         {
-            var service = new Service();
+            var service = new Service(){Id = 0};
             return View("ServiceForm", service);
         }
 
-        //POST: Services/create
-        public ActionResult Save(Service service)
+        // PUT: Services/edit
+       
+        public async Task<ActionResult> Edit(int id)
         {
-            _context.Services.Add(service);
+            var service = await _context.Services.SingleOrDefaultAsync(s => s.Id == id);
+            return View("ServiceForm", service);
+        }
+
+        // GET: Services
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            var services = await _context.Services.ToListAsync();
+            return View(services);
+        }
+
+       
+
+        //POST: Services/create
+        [HttpPost]
+        public async Task<ActionResult> Save(Service service)
+        {
+            if (service.Id==0)
+            {
+                 _context.Services.Add(service);
+            }
+            else
+            {
+                var serviceInDb = await _context.Services.SingleAsync(s => s.Id == service.Id);
+                serviceInDb.Name = service.Name;
+            }
             _context.SaveChanges();
             return RedirectToAction("Index", "Services");
         }
 
+     
+        public async Task<ActionResult> Delete(int id)
+        {
+            var serviceToDelete = await _context.Services.SingleOrDefaultAsync(s => s.Id == id);
+
+            if (serviceToDelete == null)
+                return RedirectToAction("Index", "Services");
+
+            _context.Services.Remove(serviceToDelete);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Services");
+        }
     }
 }
