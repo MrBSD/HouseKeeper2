fd79c42697dd1ed195de45c7a34ca718512fe1f5
MrBSD <MrBSD82@gmail.com>
2017-11-06 09:07:33 +0200

    Added TariffsController and index page for tariffs

---
+++ HouseKeeper2/Controllers/TariffsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using HouseKeeper2.Persistence;
+
+namespace HouseKeeper2.Controllers
+{
+    public class TariffsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TariffsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        
+        // GET: Tariffs
+        public async Task<ActionResult> Index()
+        {
+            var tariffsList = await _context.Tariffs.ToListAsync();
+            return View(tariffsList);
+        }
+    }
+}
