a3c285ee548015a2c4b0b3bde9cc43088e6f8604
MrBSD <MrBSD82@gmail.com>
2017-11-06 14:54:18 +0200

    Added Counter and Measerment classes

---
+++ HouseKeeper2/Core/Models/Counter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HouseKeeper2.Core.Models
+{
+    public class Counter
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string SerialNumber { get; set; }
+
+        [Required]
+        public int CurrentMeasurementId { get; set; }
+        public Measurement CurrentMeasurement { get; set; }
+        public ICollection<Measurement> MeasurementHistory { get; set; }
+
+        public Counter()
+        {
+            MeasurementHistory = new List<Measurement>();
+        }
+
+        private void AddMeaserment(Measurement measurement)
+        {
+            CurrentMeasurementId = measurement.Id;
+            CurrentMeasurement = measurement;
+            MeasurementHistory.Add(measurement);
+        }
+    }
+}
---
+++ HouseKeeper2/Core/Models/Measurement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HouseKeeper2.Core.Models
+{
+    public class Measurement
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int CounterId { get; set; }
+        public Counter Counter { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
