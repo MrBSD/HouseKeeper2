b6b84c757f004c247e4dbd933ca2553279d75303
MrBSD <MrBSD82@gmail.com>
2017-11-07 16:11:36 +0200

    Added CountersController

---
+++ HouseKeeper2/Controllers/CountersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using HouseKeeper2.Persistence;
+
+namespace HouseKeeper2.Controllers
+{
+    public class CountersController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public CountersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Counters
+        public async Task<ActionResult> Index()
+        {
+            var viewModel = await _context.Counters
+                .Include(c => c.Measurements)
+                .ToListAsync();
+            return View(viewModel);
+        }
+    }
+}
