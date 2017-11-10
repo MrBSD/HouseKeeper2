3228035566c57d167016fae14f742b697900416e
MrBSD <MrBSD82@gmail.com>
2017-11-10 18:06:04 +0200

    Added ServiceBill class

---
+++ HouseKeeper2/Core/Models/ServiceBill.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HouseKeeper2.Core.Models
+{
+    public class ServiceBill
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ServiceId { get; set; }
+        public Service Service { get; set; }
+
+        [Required]
+        public int TariffId { get; set; }   
+        public Tariff Tariff { get; set; }
+
+        [Required]
+        public int Month { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+        public decimal TotalForPayment { get; set; }
+    }
+}
